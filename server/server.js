dea971980f744247ad9edb9efa55a6f7438c741d
mbrankincrossvale <mbrankin@me.com>
2018-08-03 00:41:52 +0100

    added client and server files

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+
+
+app.use('/static/', express.static(path.join(__dirname, '../client-app/build/static/')))
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/../client-app/build/index.html'));
+});
+
+
+app.listen(4200, function() {
+    console.log('app has started listening.');
+});
