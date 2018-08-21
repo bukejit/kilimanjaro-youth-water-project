a7923a9a09e2ba151f143aa38749e12d4d5c3463
Shane Jordan <shane.jordan@3en.cloud>
2018-08-21 15:25:50 +0100

    Added a basic navbar - updated bootstrap to 4

--- client-app/src/components/App.js
+++ client-app/src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Header from './common/Header';
+import SecondHeader from './common/SecondHeader';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        {this.props.children}
+        <SecondHeader />
+        <div className="push">
+        { this.props.children }
+        </div>
       </div>
     );
   }
---
+++ client-app/src/components/common/SecondHeader.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './SecondHeader.css';
+
+const SecondHeader = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-success fixed-top customNav">
+    <div className="container-fluid header-container">
+        <Link to="/" className="navbar-brand logo" activeClassName="active">
+          <img src="http://placehold.it/300x60?text=Logo" width="150" height="30" alt="LOGO"></img>
+        </Link>
+        <h3 className="text-light">Kilimanjaro Youth Water Project</h3>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse custBreadcrumbs" id="collapsibleNavbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/Home" className="navbar-brand logo" activeClassName="active">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/About" className="navbar-brand logo" activeClassName="active">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="navbar-brand logo" activeClassName="active">Blogs</Link>
+            </li>
+            <li className="donate-button">
+              <button type="button" className="btn btn-primary btn-lg">Donate</button>
+            </li>
+          </ul>
+        </div>
+    </div>
+  </nav>
+);
+
+export default SecondHeader;
